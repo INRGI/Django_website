a91a1830151b480ad0c36432bf0d765f687e2e53
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-09-20 20:00:45 +0200

    Update settings.py
    delete information about smtp

--- website/settings.py
+++ website/settings.py
@@ -88,18 +88,14 @@ WSGI_APPLICATION = "website.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
 }
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.####'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'qsgsmtp@gmail.com'
+EMAIL_HOST_USER = '############'
 EMAIL_HOST_PASSWORD = '-_-'
 
 EMAIL_SERVER = EMAIL_HOST_USER
